5893e3cda0a4d8bdd00bb7aacaec93e1aeed2ffb
humphrey <humphrey.codrington@zoopla.co.uk>
2021-02-04 13:12:19 +0000

    operators

---
+++ javascript-day-basics/assignment-arithmetic-operators/multiplcation.js
@@ -0,0 +1,10 @@
+
+// multiplication
+
+console.log(3 * 4);
+
+console.log(-5 * 5);
+
+console.log("10" * 5);
+
+console.log("something" * 10);
--- lists/convert-link-list-to-string.js
+++ lists/convert-link-list-to-string.js
@@ -1,19 +1,25 @@
 function stringify(list) {
-    //go through each node in tlist and get the value out
-      let ListArray = [];
-      let str = '';
-      if (list) {
-      while (list !== null) {
-        ListArray.push(list.data);
-        list = list.next;
-        str = ListArray.join(' -> ') + ' -> null';
-        }
-      } else{ ListArray = [] 
-        return 'null'
-        } 
-      return str
-      console.log(`'${str}'`)
+//go through each node in the list and get the value out to use methods on
+    let ListArray = [];
+    let str = '';
+//  if list argument is passed and is present and thus true do a while loop 
+//while list is not = to null push each .date part of the L.list into an array
+    if (list) {
+    while (list !== null) {
+    ListArray.push(list.data);
+//update the list tp = list .next on the linked list and loop through and add to array until null
+    list = list.next;
+//update empty sting with List array items using join method and concatioation
+    str = ListArray.join(' -> ') + ' -> null';
     }
+// if list array is empty return null
+    } else { ListArray = [] 
+    return 'null'
+    } 
+//when all is finished return the new string    
+    return str
+    console.log(`'${str}'`)
+};
 
     //   tried to create a for each loop that does a function create arrows
     //takes array and index (counter) called an item which you can act upon
@@ -23,16 +29,16 @@ function stringify(list) {
     //     arr[index] = item * 2;
     //   }
     //}
-    
-    
-    function stringifyRecursion(list)
-      if (list) {
-       return String(list.data) + ' -> ' + stringify(list.next);
-      } else {
-       return 'null'
-     }
+//recursion methods
+   
+function stringifyRecursion(list)
+    if (list) {
+    return String(list.data) + ' -> ' + stringify(list.next);
+    } else {
+    return 'null'
+};
 
-    function stringifyRecursionSecondMethod(list) {
-        return list === null ? "null" : `${list.data} -> ${stringify(list.next)}`; 
-      }
-      
+function stringifyRecursionSecondMethod(list) {
+    return list === null ? "null" : `${list.data} -> ${stringify(list.next)}`; 
+};
+     
