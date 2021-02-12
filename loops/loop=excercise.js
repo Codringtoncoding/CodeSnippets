defefec62b19e3a0ca3414946b2367a0ff7c4446
humphrey <humphrey.codrington@zoopla.co.uk>
2021-02-12 17:11:11 +0000

    added dom work

--- loops/loop=excercise.js
+++ loops/loop=excercise.js
@@ -36,38 +36,65 @@ const person = {
   dob: "",
 };
 
-let arrayOfPeople = [];
+const arrayOfPeople = [
+  {
+    name: "ben",
+    favorite_color: "blue",
+    dob: "1998",
+  },
+  {
+    name: "jame",
+    favorite_color: "green",
+    dob: "1909",
+  },
+  {
+    name: "alex",
+    favorite_color: "blue",
+    dob: "2010",
+  },
+];
+
+function displayPeople(arrayOfPeople) {
+     console.log('---------------')
+  for (const person of arrayOfPeople) {
+    for (const item in person) {
+        console.log(`${item}: ${person[item]}`);
+        console.log('---------------')
+
+    }
+  }
+}
+
+displayPeople(arrayOfPeople);
 
 function pushPeopleToARray(personInArray) {
-
+ console.log('---------------')
   arrayOfPeople.push(personInArray);
-  for (const element of arrayOfPeople) {
-    console.log(element);
-  }
-  for (const property in personInArray) {
-    console.log(`${property}: ${personInArray[property]}`);
 
+  for (const person of arrayOfPeople) {
+    for (const item in person) {
+        console.log(`${item}: ${person[item]}`);
+
+      }
   }
 
 }
+// const person1 = {...person};
+// person1.name = "james";
+// person1.favorite_color = "blue";
+// person1.dob = "2010";
+// pushPeopleToARray(person1);
 
+// const person2 = {...person};
+// person2.name = "chad";
+// person2.favorite_color = "green";
+// person2.dob = "2012";
+// pushPeopleToARray(person2);
 
-const person1 = {...person};
-person1.name = "james";
-person1.favorite_color = "blue";
-person1.dob = "2010";
-pushPeopleToARray(person1);
-
-const person2 = {...person};
-person2.name = "chad";
-person2.favorite_color = "green";
-person2.dob = "2012";
-pushPeopleToARray(person2);
-
-const person3 = {...person};
-person3.name = "ben";
-person3.favorite_color = "red";
-person3.dob = "2011";
-pushPeopleToARray(person3);
+// const person3 = {...person};
+// person3.name = "ben";
+// person3.favorite_color = "red";
+// person3.dob = "2011";
+// pushPeopleToARray(person3);
 
 console.log(arrayOfPeople)
